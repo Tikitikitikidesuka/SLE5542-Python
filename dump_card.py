afd99e203512e79574a5a70ed087481835be5c20
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2024-10-18 12:34:38 +0200

    Add card dump example script

---
+++ dump_card.py
@@ -0,0 +1,25 @@
+from smartcard.System import readers
+from smartcard.util import toHexString, toASCIIString
+
+from SLE5542 import SLE5542
+
+def main():
+    try:
+        reader = readers()[0]
+        connection = reader.createConnection()
+        connection.connect()
+        card = SLE5542(connection)
+
+        card.select()
+        for line in range(0x10):
+            data, ok = card.read(0x10 * line, 0x10)
+            print(toHexString(data) + " -> " + toASCIIString(data))
+    except Exception as e:
+        print(f"Error reading card: {e}")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
